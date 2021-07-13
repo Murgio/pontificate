77ddd862f68d3bdbe4921cd77d61dd4ec0c69922
Muriz Serifovic <muriz.se@gmail.com>
2021-07-13 17:45:09 +0200

    Add words posts

---
+++ webapp/words.py
@@ -0,0 +1,176 @@
+from flask import Blueprint
+from flask import flash
+from flask import g
+from flask import redirect
+from flask import render_template
+from flask import request
+from flask import url_for
+from werkzeug.exceptions import abort
+
+from wrods.db import get_db
+
+bp = Blueprint("words", __name__)
+
+
+@bp.route("/")
+def index():
+    """Show all the posts, most recent first."""
+    db = get_db()
+    posts = db.execute(
+        "SELECT p.id, title, body, created, author_id, username"
+        " FROM post p"
+        " ORDER BY created DESC"
+    ).fetchall()
+    return render_template("index.html", posts=posts)
+
+
+def get_post(id, check_author=True):
+    """Get a post and its author by id.
+    Checks that the id exists and optionally that the current user is
+    the author.
+    :param id: id of post to get
+    :param check_author: require the current user to be the author
+    :return: the post with author information
+    :raise 404: if a post with the given id doesn't exist
+    :raise 403: if the current user isn't the author
+    """
+    post = (
+        get_db()
+        .execute(
+            "SELECT p.id, title, body, created, author_id, username"
+            " FROM post p JOIN user u ON p.author_id = u.id"
+            " WHERE p.id = ?",
+            (id,),
+        )
+        .fetchone()
+    )
+
+    if post is None:
+        abort(404, f"Post id {id} doesn't exist.")
+
+    if check_author and post["author_id"] != g.user["id"]:
+        abort(403)
+
+    return post
+
+
+@bp.route("/create", methods=("GET", "POST"))
+def create():
+    """Create a new post for the current user."""
+    if request.method == "POST":
+        title = request.form["title"]
+        body = request.form["body"]
+        error = None
+
+        if not title:
+            error = "Title is required."
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                "INSERT INTO post (title, body, author_id) VALUES (?, ?, ?)",
+                (title, body, g.user["id"]),
+            )
+            db.commit()
+            return redirect(url_for("blog.index"))
+
+    return render_template("blog/create.html")
+
+
+@bp.route("/<int:id>/update", methods=("GET", "POST"))
+def update(id):
+    """Update a post if the current user is the author."""
+    post = get_post(id)
+
+    if request.method == "POST":
+        title = request.form["title"]
+        body = request.form["body"]
+        error = None
+
+        if not title:
+            error = "Title is required."
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                "UPDATE post SET title = ?, body = ? WHERE id = ?", (title, body, id)
+            )
+            db.commit()
+            return redirect(url_for("blog.index"))
+
+    return render_template("blog/update.html", post=post)
+
+
+@bp.route("/<int:id>/delete", methods=("POST",))
+def delete(id):
+    """Delete a post.
+    Ensures that the post exists and that the logged in user is the
+    author of the post.
+    """
+    get_post(id)
+    db = get_db()
+    db.execute("DELETE FROM post WHERE id = ?", (id,))
+    db.commit()
+    return redirect(url_for("blog.index"))
+
+#**************************************************************************************************************
+
+from flask import Flask, render_template, request, Blueprint
+import sqlite3
+
+app = Flask(__name__)
+
+DB = "mienfoo.db"
+
+def get_db_connection():
+    conn = sqlite3.connect(DB)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+@app.route('/')
+@app.route('/index.html', methods = ['POST', 'GET'])
+def index():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("select * from words")
+    rows = cur.fetchall();
+    return render_template('index.html', rows=rows)
+
+@app.route('/create')
+def new_word():
+    ...
+   #return render_template('student.html')
+
+@app.route('/addrec',methods = ['POST', 'GET'])
+def addrec():
+   if request.method == 'POST':
+      try:
+         name = request.form['name']
+         definition = request.form['definition']
+         with sql.connect(DB) as con:
+            cur = con.cursor()
+            cur.execute("INSERT INTO students (name,addr,city,pin) VALUES (?,?,?,?)",(nm,addr,city,pin) )
+            con.commit()
+            msg = "Record successfully added"
+      except:
+         con.rollback()
+         msg = "error in insert operation"
+      finally:
+         return render_template("result.html",msg = msg)
+         con.close()
+
+@app.route('/list')
+def list():
+   con = sql.connect("database.db")
+   con.row_factory = sql.Row
+   cur = con.cursor()
+   cur.execute("select * from students")
+   rows = cur.fetchall();
+   return render_template("list.html",rows = rows)
+
+if __name__ == '__main__':
+   app.run(debug = True, host='0.0.0.0', port=3141)
